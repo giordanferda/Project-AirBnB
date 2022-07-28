1cd5459bfe3fa850085953a806f00f0b4548baaf
usernameGio <giordan.maniti@gmail.com>
2022-07-28 18:43:29 -0400

    logout

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,17 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 
 
